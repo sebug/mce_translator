cad654f81291e5aa62c6a3c2accd7dcc914f09a3
sebug <sebastian.gfeller@gmail.com>
2014-01-19 21:18:38 +0100

    Now correctly handling escaped elements

--- mce_translator.fs
+++ mce_translator.fs
@@ -5,11 +5,27 @@ open FSharp.Data
 
 type TranslationLines = CsvProvider<"example_input.csv">
 
+exception EndGroupNotFoundError of string
+exception PathSeparatorNotFoundError of string
+
+let rec escaped_element (pstr: string) (acc: string) : string * string =
+    if pstr.Length = 0 then raise (EndGroupNotFoundError("Did not find ]"))
+    else
+        match pstr.[0] with
+        | ']' -> (acc, pstr.Substring(1))
+        | c -> escaped_element (pstr.Substring(1)) (acc + c.ToString())
+
 let rec path_element (pstr: string) (acc: string) : string * string =
     if pstr.Length = 0 then (acc, "")
     else
         match pstr.[0] with
         | '.' -> (acc, pstr.Substring(1))
+        | '[' ->
+            let p = escaped_element (pstr.Substring(1)) ""
+            match p with
+            | (eel,"") -> (eel,"")
+            | (eel,more) -> if more.[0] = '.' then (eel, more.Substring(1))
+                            else raise (PathSeparatorNotFoundError("Did not find ."))
         | c -> path_element (pstr.Substring(1)) (acc + c.ToString())
 
 let rec path_elements (pstr: string) : string list =
